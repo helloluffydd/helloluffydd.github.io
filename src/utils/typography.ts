23be9d350ce1887f0dd787b59b8d72d7efe389ea
Luffy Chen <luffychen0715@gmail.com>
2022-01-22 02:36:19 +0800

    fix: add GenSekiGothicTW and GenWanMin fonts and set up font-face

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -3,8 +3,8 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
-  bodyFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
+  headerFontFamily: ['GenWanMin TW SemiBold', 'Nanum Gothic'],
+  bodyFontFamily: ['GenSekiGothic Regular', 'Nanum Gothic'],
 });
 
 export default typography;
