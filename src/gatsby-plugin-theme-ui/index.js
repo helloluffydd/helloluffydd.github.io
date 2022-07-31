581646ee48aed255d1c83c0082a13d85b06193a1
Luffy Chen <luffychen0715@gmail.com>
2022-07-31 19:30:45 +0800

    fix: revise theme background colors

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,11 +1,11 @@
 export default {
   colors: {
     text: '#333', // '#24292e'
-    background: '#f2dec9', // '#f1f1f1'
+    background: '#f2e7de', // '#f1f1f1'
     modes: {
       dark: {
-        text: '#dddddd',
-        background: '#141414',
+        text: '#eeeeee',
+        background: '#041821',
       },
     },
   },
